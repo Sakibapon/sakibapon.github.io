f961b16481afc97af4d6b787b556061e661bb0ef
Sakib <63115441+Sakibapon@users.noreply.github.com>
2022-08-22 03:15:15 +0600

    php update

---
+++ _pages/skill.php
@@ -0,0 +1,64 @@
+   <!-- ======= Skills Section ======= -->
+   <section id="skills" class="about section-bg" style="background-color: #E1EBFE">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Skills</h2>
+      </div>
+        <div class="row">
+            <div class="col content" data-aos="fade-left">
+              <p><strong>Languages</strong></p>
+              <ul > 
+                <li><i class="icofont-rounded-right"></i> <strong>Python</strong>  </li>
+                <li><i class="icofont-rounded-right"></i> <strong>C++ </strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>C# </strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Java </strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Javascipt </strong> </li>
+              </ul>
+            </div>
+
+            <div class="col content" data-aos="fade-left">
+              <p><strong>AI</strong></p>
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Machine Learning</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Deep Learning</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Computer Vision</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>NLP</strong> </li>
+              </ul>
+            </div>
+
+            <div class="col content" data-aos="fade-left">
+              <p><strong>Library</strong></p>
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Pandas</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>NumPy</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Tensorflow</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Keras</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>PyTorch</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Open3D</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Scikit-learn</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Matplotlib</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Seaborn</strong> </li>
+              </ul>
+            </div>
+            <div class="col content" data-aos="fade-left">
+              <p><strong>Web Programming</strong></p>
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>HTML</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>CSS</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Javascript</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>PHP</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>MYSQL</strong> </li>
+              </ul>
+            </div>
+            <div class="col content" data-aos="fade-left">
+              <p><strong>Web Programming Frameworks</strong></p>
+              <ul>
+                <li><i class="icofont-rounded-right"></i> <strong>Bootstraps</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>React</strong> </li>
+                <li><i class="icofont-rounded-right"></i> <strong>Node.js</strong> </li>
+              </ul>
+            </div>
+        </div>
+    </div>
+  </section><!-- End Skills Section -->
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php $title = "Sakib Apon"; 
+   include ('includes/header.php') ; ?>
+
+    <!-- ======= Intro Page ======= -->
+    <!-- <?php include ('_pages/intro.php') ; ?> -->
+
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+        <div class="container">
+  
+          <div class="section-title">
+            <h2>Welcome</h2>
+  
+          </div>
+  
+          <div class="row">
+            <div class="col-lg-4" data-aos="fade-right">
+              <img src="assets/img/pp1.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+              <h3>About</h3>
+              <p>
+                I am a Research Associate/ R&D Software Engineer working on AI and Machine Learning. 
+                Currently, I am appointed as a Research Associate in <a href="https://www.bracu.ac.bd" target="_blank">BRAC University</a>. 
+              </p>
+              <p>
+                My research focuses on general AI & Machine Learning.
+                I use various statistical data science technology such as machine learning/deep learning to solve 
+                issues in the field of Autonomous systems, Edge Computing, Computer Vision/Graphics, Natural Language Processing, etc. 
+                Additionally, I am quite interested in interpretable machine learning.
+              </P>
+              <p>
+                I obtained my BSc in <a href="https://www.bracu.ac.bd/academics/departments/computer-science-and-engineering/bachelor-science-computer-science-and" target="_blank">Computer Science and Engineering<a> 
+                at the <a href="https://www.bracu.ac.bd" target="_blank">BRAC University</a>, Dhaka, Bangladesh,  
+                where I developed a methodological toolkit to understand complex policy problems with applications to climate change and 
+                illicit finance. While I was at BAC, I was affiliated with Professor 
+                <a href="https://www.bracu.ac.bd/about/people/md-golam-rabiul-alam-phd" target="_blank">Md. Golam Rabiul Alam</a>. Under his
+                supervison I have authored or coauthored several articles in prestigious conferences and journals.
+              </p>
+
+              <p class="font-italic">
+                 Connect with Me 
+              </p>
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <a href="https://www.linkedin.com/in/sakibapon/" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" height=25>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.github.com/sakibapon" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/github-F1EFEC?style=for-the-badge&logo=github&logoColor=black" height=25>
+                      </a> 
+                    </li>
+                    <li>
+                      <a  href="tel:+8801951445484" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/Phone-0AA530?style=for-the-badge&logo=call&logoColor=white" height=25>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="mailto:sakibapon07@gmail.com" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" height=25>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul>
+                    <li>
+                      <a href="https://scholar.google.com/citations?user=Y9KajaYAAAAJ&hl=en&authuser=1" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/GOOGLE%20SCHOALAR-A0C3FF?style=for-the-badge&logo=googlescholar&logoColor=blue" height=25>
+                      </a>
+                    </li>                    
+                    <li>
+                        <a href="https://www.researchgate.net/profile/Sakib-Apon" target="_blank"><i class="icofont-rounded-right"></i>
+                            <img src="https://img.shields.io/badge/Research%20Gate-00A39E?style=for-the-badge&logo=researchgate&logoColor=white" height=25>
+                        </a>
+                    </li>
+                    <li>
+                      <a href="https://orcid.org/0000-0002-5238-8276" target="_blank"><i class="icofont-rounded-right"></i>
+                        <img src="https://img.shields.io/badge/Orcid-a6ce39?style=for-the-badge&logo=orcid&logoColor=white" height=25>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <p>
+  
+              </p>
+            </div>
+          </div>
+  
+        </div>
+      </section><!-- End About Section -->
+
+
+    <!-- ======= Certificates ======= -->
+     <?php include ('certificates.php') ; ?>
+
+
+    <!-- ======= Contact ======= -->
+    <?php include ('contact.php') ; ?>
+
+<?php include ('includes/footer.php') ; ?>
