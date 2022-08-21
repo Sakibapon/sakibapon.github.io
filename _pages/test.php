38d5fd064b3b0a1ef61e2d05b4b57af6a5ad2aa4
Sakib <63115441+Sakibapon@users.noreply.github.com>
2022-08-22 00:02:21 +0600

    php update

---
+++ _pages/test.php
@@ -0,0 +1,5 @@
+<?php $title = "Education & Experience"; 
+   include ('../header.php') ; ?>
+  
+<?php $title = "Education & Experience"; 
+   include ('../footer.php') ; ?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php $title = "Sakib Apon"; 
+   include ('header.php') ; ?>
+
+    <!-- ======= Intro Page ======= -->
+    <!-- <?php include ('_pages/intro.php') ; ?> -->
+
+    <!-- ======= About ======= -->
+    <?php include ('_pages/about.php') ; ?>
+
+    <!-- ======= Education & Experience ======= -->
+    <?php include ('_pages/education.php') ; ?>
+
+    <!-- ======= Projects ======= -->
+    <?php include ('_pages/projects.php') ; ?>
+
+    <!-- ======= Publications ======= -->
+    <?php include ('_pages/publications.php') ; ?>
+
+    <!-- ======= Certificates ======= -->
+     <?php include ('_pages/certificates.php') ; ?>
+
+    <!-- ======= Skill ======= -->
+    <?php include ('_pages/skill.php') ; ?>
+
+    <!-- ======= Testimonials ======= -->
+    <?php include ('_pages/testimonials.php') ; ?>
+
+    <!-- ======= Contact ======= -->
+    <?php include ('_pages/contact.php') ; ?>
+
+<?php include ('footer.php') ; ?>
